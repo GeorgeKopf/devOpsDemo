c49bede02cacce5f4db0efdf5d4e0eab1dc82f2e
Dad-PC\Dad <george.kopf@gmail.com>
2017-02-14 20:21:59 -0500

    Initial Commit

---
+++ src/test/java/com/kopf/LoginBeanTest.java
@@ -0,0 +1,17 @@
+package com.kopf;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Dad on 2/14/2017.
+ */
+public class LoginBeanTest extends TestCase {
+
+    public void testPasswordValidation() {
+       LoginBean myTestValidate = new LoginBean();
+       myTestValidate.setPassword("admin");
+       assertEquals("admin password will return true", true, myTestValidate.validate());
+       myTestValidate.setPassword("asdfcg");
+       assertEquals("junk password will return false", false, myTestValidate.validate());
+    }
+}
